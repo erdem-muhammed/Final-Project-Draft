27dfe2f73c85700227dec1e72f14636a30cd9480
muhammed erdem <erdem.muhammed1@outlook.com>
2021-12-20 22:31:26 +0100

    connection created

--- register.php
+++ register.php
@@ -7,4 +7,10 @@ if (isset($_SERVER['HTTP_ORIGIN']))
     header('Access-Control-Max-Age: 86400');
 }
 
+$server_name = "localhost";
+$server_user = "root";
+$server_pass = "";
+$database_name = "final_project";
+
+$conn = new mysqli($server_name, $server_user, $server_pass, $database_name);
 ?>
