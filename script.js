88cd96a54728138e570bfd8908cfed2cebbeb82a
muhammed erdem <erdem.muhammed1@outlook.com>
2021-12-30 20:22:00 +0100

    new FormData

--- script.js
+++ script.js
@@ -5,5 +5,7 @@ const myForm = document.getElementById("registerForm");
 
 myForm.addEventListener('submit', function(e){
     e.preventDefault();
-    
-})
+
+    const formData = new FormData(this);
+
+});
