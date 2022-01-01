088797044411c6f86790e2a2464c5142bc14a16f
muhammed erdem <erdem.muhammed1@outlook.com>
2022-01-01 19:52:51 +0100

    id added to both inp

--- login.js
+++ login.js
@@ -2,7 +2,7 @@
 const loginInp = document.getElementById("loginInp");
 
 const formData = new FormData();
-formData.append
+formData.append("email", )
 
 loginInp.addEventListener('submit', function() {
     fetch('http://localhost/project_final/database/login.php', {
