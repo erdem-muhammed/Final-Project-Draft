0ccccd637b1fb714f5c14bb14de41132eb348025
muhammed erdem <erdem.muhammed1@outlook.com>
2022-01-01 19:51:49 +0100

    formdata created

--- login.js
+++ login.js
@@ -1,6 +1,9 @@
 
 const loginInp = document.getElementById("loginInp");
 
+const formData = new FormData();
+formData.append
+
 loginInp.addEventListener('submit', function() {
     fetch('http://localhost/project_final/database/login.php', {
         method: 'POST',
