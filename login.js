568cdfed0f008b300e844c0b6fbbea9c5d8ae89b
muhammed erdem <erdem.muhammed1@outlook.com>
2022-01-01 19:54:02 +0100

    pass append

--- login.js
+++ login.js
@@ -3,7 +3,7 @@ const loginInp = document.getElementById("loginInp");
 
 const formData = new FormData();
 formData.append("email", document.getElementById("usernameInp"));
-
+formData.append("password", document.getElementById("passInp"));
 
 loginInp.addEventListener('submit', function() {
     fetch('http://localhost/project_final/database/login.php', {
