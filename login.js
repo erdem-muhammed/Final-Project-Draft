e968ff493f4ddbea02152c33ce140252723188a4
muhammed erdem <erdem.muhammed1@outlook.com>
2022-01-01 19:49:36 +0100

    fetch added

--- login.js
+++ login.js
@@ -2,5 +2,9 @@
 const loginInp = document.getElementById("loginInp");
 
 loginInp.addEventListener('submit', function() {
-    
+    fetch('http://localhost/project_final/database/login.php', {
+        method: 'POST',
+        mode: 'cors',
+        
+    })
 })
